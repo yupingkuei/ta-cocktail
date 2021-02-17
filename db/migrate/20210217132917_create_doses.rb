0d409ef461c68589f7f034d076dc847c76d573fa
Yuping Kuei <kueiyuping@gmail.com>
2021-02-18 00:17:15 +0900

    fixed the references name in table dose and set the valiations and table relations

--- db/migrate/20210217132917_create_doses.rb
+++ db/migrate/20210217132917_create_doses.rb
@@ -2,7 +2,7 @@ class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
       t.string :description
-      t.references :dose, null: false, foreign_key: true
+      t.references :cocktail, null: false, foreign_key: true
       t.references :ingredient, null: false, foreign_key: true
 
       t.timestamps
